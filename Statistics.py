2b597710f75ced6211e17cca98d9401923f69833
rachaelthormann <rachaelthormann@gmail.com>
2019-08-03 19:21:25 -0400

    Added functionality to obtain averages by month.

--- Statistics.py
+++ Statistics.py
@@ -6,6 +6,10 @@ Last Edited: 8/2/2019
 File Purpose: To process data and produce statistics.
 """
 
+import pandas as pd
+import numpy as np
+import statistics
+
 
 def printStatistics(df):
     """
@@ -27,4 +31,25 @@ def printStatistics(df):
     # print the day that has the minimum average
     print(df.mean().idxmin())
     print(df.mean().min())
+
+    # transpose so that date is functioning as index
+    dfT = df.T
+    # convert index to datetime
+    dfT.index = pd.to_datetime(dfT.index)
+
+    print("\n")
+
+    months = dfT.groupby(pd.Grouper(freq="M")).mean().index
+    month_counter = 0
+
+    # print each month with its associated average
+    for x in dfT.groupby(pd.Grouper(freq="M")).mean().values:
+        print(str(months[month_counter].strftime('%B %Y')) + ":  " + str(np.nanmean(x).round(2)))
+        month_counter += 1
+
+
+
+
+
+
 
